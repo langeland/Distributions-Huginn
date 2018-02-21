80af80aa52405331604bf9608bb9f15288dbe001
mdha <mdha@dr.dk>
2018-02-21 16:28:50 +0100

    Changed jira to validation and extended to handle branch name validation

---
+++ Packages/Application/Langeland.Huginn/Classes/Controller/ValidationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Langeland\Huginn\Controller;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+use Langeland\Huginn\Service\JiraService;
+
+
+class ValidationController extends ActionController
+{
+
+    /**
+     * @var JiraService
+     * @Flow\Inject
+     */
+    protected $jiraService;
+
+    /**
+     * Checks branch naming and if jira issue exists
+     * @return string
+     */
+    public function isValidBranchAction()
+    {
+        $branchName = $this->request->getArgument('branchName');
+        $issueKey = $this->request->getArgument('issueKey');
+
+        if (empty($branchName) || !preg_match('/^(feature|task|bugfix|hotfix)$/', $branchName)) {
+
+            $this->response
+                ->setStatus(400);
+            return "ERROR: Invalid branch: '$branchName' \nBranch must begin with 'feature/', 'task/', 'bugfix/' or 'hotfix/' and be all lowercase";
+
+        } elseif (empty($issueKey) || !$this->jiraService->hasIssue($issueKey)) {
+
+            $this->response
+                ->setStatus(404);
+            return "ERROR: Jira issue not found with key: '$issueKey' \nBranch must end with a issue key corresponding to an existing issue in Jira";
+
+        }
+
+        $this->response
+            ->setStatus(200);
+
+        return '';
+
+    }
+
+
+}
