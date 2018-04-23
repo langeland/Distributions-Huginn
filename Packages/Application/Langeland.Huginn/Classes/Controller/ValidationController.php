66e7aba3fb3d52da67015e8b489ccf2fa09490c6
Jon Klixbüll Langeland <langeland@users.noreply.github.com>
2018-04-23 09:08:27 +0200

    Refacture isValidBranch (#5)

--- Packages/Application/Langeland.Huginn/Classes/Controller/ValidationController.php
+++ Packages/Application/Langeland.Huginn/Classes/Controller/ValidationController.php
@@ -11,6 +11,11 @@ class ValidationController extends ActionController
 {
 
     /**
+     * @var string
+     */
+    protected $defaultViewObjectName = \Neos\Flow\Mvc\View\JsonView::class;
+
+    /**
      * @var JiraService
      * @Flow\Inject
      */
@@ -18,37 +23,54 @@ class ValidationController extends ActionController
 
     /**
      * Checks branch naming and if jira issue exists
-     * @return string
+     * @param string $branchName
+     * @return void
      */
-    public function isValidBranchAction()
+    public function isValidBranchAction(string $branchName)
     {
-        $branchName = $this->request->getArgument('branchName');
-        $issueKey = $this->request->getArgument('issueKey');
-        if(preg_match('/^(experimental)$/', $branchName)){
-            $this->response
-                ->setStatus(200);
-            return "";
-        }
-        elseif (!preg_match('/^(feature|task|bugfix|hotfix)$/', $branchName)) {
 
-            $this->response
-                ->setStatus(400);
-            return "ERROR: Invalid branch: '$branchName' \nBranch must begin with 'feature/', 'task/', 'bugfix/', 'hotfix/' or 'experimental' and be all lowercase";
+        $branchParts = explode('/', $branchName, 3);
 
-        } elseif (empty($issueKey) || !$this->jiraService->hasIssue($issueKey)) {
-
-            $this->response
-                ->setStatus(404);
-            return "ERROR: Jira issue not found with key: '$issueKey' \nBranch must end with a issue key corresponding to an existing issue in Jira";
-
+        if (count($branchParts) <= 1) {
+            $this->view->assign('value', [
+                'status' => 'ERROR',
+                'description' => 'Branch name must contain a minimum of two parts',
+                'branchName' => $branchName
+            ]);
+            return;
         }
 
-        $this->response
-            ->setStatus(200);
+        if (in_array($branchParts[0], ['feature', 'task', 'bugfix', 'hotfix', 'experimental']) === false) {
+            $this->response->setStatus(400);
+            $this->view->assign('value', [
+                'status' => 'ERROR',
+                'description' => 'Invalid branch: ' . $branchParts[0] . '. Branch must begin with feature/, task/, bugfix/, hotfix/ or experimental and be all lowercase',
+                'branchName' => $branchName
+            ]);
+            return;
+        }
 
-        return "";
+        if ($branchParts[0] === 'experimental') {
+            $this->response->setStatus(200);
+            $this->view->assign('value', ['status' => 'OK', 'branchName' => $branchName]);
+            return;
+        }
+
+        if ($this->jiraService->hasIssue($branchParts[1]) === false) {
+
+            $this->response->setStatus(404);
+            $this->view->assign('value', [
+                'status' => 'ERROR',
+                'description' => 'Jira issue not found with key: ' . $branchParts[1] . '. Branch must end with a issue key corresponding to an existing issue in Jira',
+                'branchName' => $branchName
+            ]);
+            return;
+
+        }
+
+        $this->response->setStatus(200);
+        $this->view->assign('value', ['status' => 'OK']);
 
     }
-
 
 }
